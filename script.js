bd64f012e257fcb9aadf696c1b69d1a1ed2c752f
Eli <109231330+theLadEli@users.noreply.github.com>
2024-04-17 15:45:31 +0100

    Build out localStorage logic
    Now moving on to task algorithm

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var userDetails = {
-    name: "",
     project_type: "",
     planning_status: "",
     project_design: "",
@@ -13,22 +12,39 @@ var userDetails = {
 
 function startForm(event) {
     event.preventDefault();
-    userDetails.name = $("input[name='name'").val();
-    localStorage.setItem('userDetails', JSON.stringify(userDetails));
+    userName = $("input[name='name'").val();
+    localStorage.setItem('userName', userName);
     location.href = "/project-details.html";
 }
 
 function saveProjectDetails(event){
-    // event.preventDefault();
+    event.preventDefault();
 
-    for (var key in userDetails) {
-        if (userDetails.hasOwnProperty(key)) {
-            userDetails.key = $("input[name='key'").val();
-            console.log(key + ": " + userDetails[key]);
-        }
+    // Set each objects value to form selection
+    for (var property in userDetails) {
+        userDetails[property] = $(`input[name='${property}']:checked`).val();
     }
 
-    // userDetails.project_type = $("input[name='project-type'").val();
-    // userDetails.project_type = $("input[name='project-type'").val();
+    // List of Professional Help
+    var checkedProfessionalHelp = document.querySelectorAll("input[name='professional_help']:checked");
+
+    var checkedValues = [];
+
+    checkedProfessionalHelp.forEach(function(checkbox) {
+        checkedValues.push(checkbox.value);
+    });
+
+    userDetails.professional_help = checkedValues
+
+    // Log entire object and save it to Local Storage
+    console.log(userDetails)
+    localStorage.setItem('userDetails', JSON.stringify(userDetails));
+
+    // Redirect user to the Checklist
+    location.href = "/checklist.html";
 }
-saveProjectDetails()
+
+$("#local_storage_loaded").append(`
+<h1>Hi ${localStorage.getItem("userName")}</h><br>
+<p>${localStorage.getItem("userDetails")}</p>
+`)
